63aeaa2ac51d76cd49a2e24996bfc82314eeaab5
alireza_inspiron <alireza.barkhordari@gmail.com>
2021-05-09 21:16:17 -0700

    minor updates in publications and chapters

--- website/src/pages/publications.js
+++ website/src/pages/publications.js
@@ -67,7 +67,16 @@ class PublicationPage extends React.Component {
         </p>
         <h1>Chapters in Books</h1>
         <p>
-          1. Farhad Faradji, Rabab K Ward, Gary E Birch,{" "}
+          1. Ramy Hussein, Rabab Ward,{" "}
+          <strong>
+            Energy-efficient EEG monitoring systems for wireless epileptic
+            seizure detection
+          </strong>
+          , Energy Efficiency of Medical Devices and Healthcare Applications,
+          Academic. Press, pp 69-85, January 2020
+        </p>
+        <p>
+          2. Farhad Faradji, Rabab K Ward, Gary E Birch,{" "}
           <strong>
             A Self-Paced Two-State Mental Task-Based Brain-Computer Interface
             with Few EEG Channels
@@ -76,7 +85,7 @@ class PublicationPage extends React.Component {
           2019
         </p>
         <p>
-          2. Faradj, F., Ward, R., and Birch, G.,{" "}
+          3. Faradj, F., Ward, R., and Birch, G.,{" "}
           <strong>
             Using Autoregressive Models of Wavelet Bases in the Design of Mental
             Task-Based BCIs
@@ -86,24 +95,24 @@ class PublicationPage extends React.Component {
           pages, June 2013.
         </p>
         <p>
-          3. Angshul Majumdar and Rabab Ward,{" "}
+          4. Angshul Majumdar and Rabab Ward,{" "}
           <strong>Multicoil Parallel MRI</strong>, in Medical Imaging:
           Technology and Applications,EDITED BY TROY FARNCOMBE AND KRIS
           INIEWSKI, Pages 485-528, CRC Press, 2013.{" "}
         </p>
         <p>
-          4. Nezhadarya, E., Wang, Z. J. and Ward, R. K.,{" "}
+          5. Nezhadarya, E., Wang, Z. J. and Ward, R. K.,{" "}
           <strong>Image Watermarking in Higher-Order Gradient Domain</strong>,
           in Advances in Wavelet Theory and Their Applications in Engineering,
           Physics and Technology, Intech, ISBN 978-953-51-0494-0.
         </p>
         <p>
-          5. Majumdar, A., Ward, R. K., and Nasiopoulos, P.,{" "}
+          6. Majumdar, A., Ward, R. K., and Nasiopoulos, P.,{" "}
           <strong>Distributed Face Recognition</strong>, in Face Recognition:
           Methods, Applications and Technology, Nova Publishers, NY.{" "}
         </p>
         <p>
-          6. Fatourechi, M., Lv, X., Malek Esmaeili, M., Wang, J. Z., and Ward,
+          7. Fatourechi, M., Lv, X., Malek Esmaeili, M., Wang, J. Z., and Ward,
           R. K.,{" "}
           <strong>
             Image and video detection using content-based fingerprinting
@@ -112,34 +121,34 @@ class PublicationPage extends React.Component {
           Guan, Y. He and S.Y. Kung), CRC Press, pp. 460 - 485, Feb 2012.{" "}
         </p>
         <p>
-          7. Majumdar, A., and Ward, R. K.,{" "}
+          8. Majumdar, A., and Ward, R. K.,{" "}
           <strong>Compressive Classification for Face Recognition</strong> in
           Face Recognition, State of the Art in Cognitive and Computational
           Processes, in Face Recognition, Intech Publishers, Milos Oravec ed.
           pp. 47-64, 2010.
         </p>
         <p>
-          8. Majumdar, A., and Ward, R. K.,{" "}
+          9. Majumdar, A., and Ward, R. K.,{" "}
           <strong>Multiresolution Methods in Face Recognition</strong>, in Face
           Recognition, State of the Art in Cognitive and Computational
           Processes, Eds. M. S. Bartlett, K. Delac and M. Grgic, InTech
           Education and Publishing, Vienna, Austria , pp. 79-96, 2009.
         </p>
         <p>
-          9. Nasiopoulos, P., Ward, R.K., and Morse, D.,{" "}
+          10. Nasiopoulos, P., Ward, R.K., and Morse, D.,{" "}
           <strong>Adaptive Compression Coding</strong>, in Image Data
           Compression, B.V. Dasaralthy, editor, published by the IEEE Computer
           Society Press, 1995, pp. 164‑173.
         </p>
         <p>
-          10. Ward, R.K. and Saleh, B.E.,{" "}
+          11. Ward, R.K. and Saleh, B.E.,{" "}
           <strong>Deblurring Random Blur</strong>, in Selected Papers in Digital
           Image Restoration, SPIE Milestone Series, ed. M.I. Sezan, Vol. MS‑47,
           1992, a reprint book of outstanding optical engineering papers
           selected from the world literature.
         </p>
         <p>
-          11. Poon, S.S., Ward, R.K., and Palcic, B.,{" "}
+          12. Poon, S.S., Ward, R.K., and Palcic, B.,{" "}
           <strong>
             Detection and Segmentation of Nucleated Cells in Blood Smears
           </strong>
@@ -148,7 +157,7 @@ class PublicationPage extends React.Component {
           Series 911, 1990, Elsevier Science Publishers.
         </p>
         <p>
-          12. Kreidieh, R.,{" "}
+          13. Kreidieh, R.,{" "}
           <strong>Method of Estimation: Acreage Response Functions</strong> in
           Foreign Trade Regimes and Economic Development: Egypt, by Bent Hansen
           and K. Nashashibi. National Bureau of Economics Research, New York.
@@ -157,7 +166,7 @@ class PublicationPage extends React.Component {
           Appendix A.
         </p>
         <p>
-          13. Hansen, B. and Kreidieh, R.,{" "}
+          14. Hansen, B. and Kreidieh, R.,{" "}
           <strong>
             An Afghanistan‑type Model for Simulating Fiscal, Monetary and
             Foreign Exchange Policy
@@ -166,7 +175,7 @@ class PublicationPage extends React.Component {
           Berkeley, California, Paper No. 25, April 1972, 144 pp.
         </p>
         <p>
-          14. Kreidieh, R. and Tourk, K.,{" "}
+          15. Kreidieh, R. and Tourk, K.,{" "}
           <strong>Agricultural Supply Elasticities</strong>, in An Appraisal of
           Economic Development in Afghanistan, 1960‑1970, by Bent Hansen, Inst.
           of Intl. Studies, University of California, Berkeley, CA, Appendix 2,
